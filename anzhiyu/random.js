3becacc4bcad85f28f721733f18f400137c86803
youyu <2000601104@cjlu.edu.cn>
2024-01-02 00:38:46 +0800

    Site updated: 2024-01-02 00:38:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["article/fef3.html","article/65a0.html","article/ae8e.html","article/5d29.html","article/4d37.html","article/7d63.html","article/ec0.html","article/ea40.html","article/a8a8.html","article/2386.html","article/dc01.html","article/e7fe.html","article/a169.html","article/2b63.html","article/4971.html"];function toRandomPost(){
+var posts=["article/fef3.html","article/65a0.html","article/5d29.html","article/4d37.html","article/ec0.html","article/ae8e.html","article/7d63.html","article/2386.html","article/ea40.html","article/a8a8.html","article/dc01.html","article/e7fe.html","article/2b63.html","article/4971.html","article/a169.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
